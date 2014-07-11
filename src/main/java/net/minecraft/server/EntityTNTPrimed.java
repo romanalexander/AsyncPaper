ff029465df7c42cb3d21d139e5ed445a51310a98
Zach Brown <Zbob750@live.com>
2014-07-11 03:54:06 -0500

    Fix directional TNT bias

--- src/main/java/net/minecraft/server/EntityTNTPrimed.java
+++ src/main/java/net/minecraft/server/EntityTNTPrimed.java
@@ -19,11 +19,11 @@ public class EntityTNTPrimed extends Entity {
     public EntityTNTPrimed(World world, double d0, double d1, double d2, EntityLiving entityliving) {
         this(world);
         this.setPosition(d0, d1, d2);
-        float f = (float) (Math.random() * 3.1415927410125732D * 2.0D);
+        //float f = (float) (Math.random() * 3.1415927410125732D * 2.0D); // PaperSpigot - Fix directional TNT bias
 
-        this.motX = (double) (-((float) Math.sin((double) f)) * 0.02F);
+        this.motX = 0; // PaperSpigot - Fix directional TNT bias //(double) (-((float) Math.sin((double) f)) * 0.02F);
         this.motY = 0.20000000298023224D;
-        this.motZ = (double) (-((float) Math.cos((double) f)) * 0.02F);
+        this.motZ = 0; // PaperSpigot - Fix directional TNT bias //(double) (-((float) Math.cos((double) f)) * 0.02F);
         this.fuseTicks = 80;
         this.lastX = d0;
         this.lastY = d1;
