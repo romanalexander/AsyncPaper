eae7935a72ec72e106a3b874c78cab4efa918ed3
Thinkofdeath <thinkofdeath@spigotmc.org>
2014-09-01 16:47:48 +1000

    Snapshot imports

---
+++ src/main/java/net/minecraft/server/PacketPlayInUpdateSign.java
@@ -0,0 +1,58 @@
+package net.minecraft.server;
+
+import java.io.IOException;
+
+public class PacketPlayInUpdateSign extends Packet {
+
+    private int a;
+    private int b;
+    private int c;
+    private String[] d;
+
+    public PacketPlayInUpdateSign() {}
+
+    public void a(PacketDataSerializer packetdataserializer) throws IOException {
+        this.a = packetdataserializer.readInt();
+        this.b = packetdataserializer.readShort();
+        this.c = packetdataserializer.readInt();
+        this.d = new String[4];
+
+        for (int i = 0; i < 4; ++i) {
+            this.d[i] = packetdataserializer.c(15);
+        }
+    }
+
+    public void b(PacketDataSerializer packetdataserializer) throws IOException {
+        packetdataserializer.writeInt(this.a);
+        packetdataserializer.writeShort(this.b);
+        packetdataserializer.writeInt(this.c);
+
+        for (int i = 0; i < 4; ++i) {
+            packetdataserializer.a(this.d[i]);
+        }
+    }
+
+    public void a(PacketPlayInListener packetplayinlistener) {
+        packetplayinlistener.a(this);
+    }
+
+    public int c() {
+        return this.a;
+    }
+
+    public int d() {
+        return this.b;
+    }
+
+    public int e() {
+        return this.c;
+    }
+
+    public String[] f() {
+        return this.d;
+    }
+
+    public void handle(PacketListener packetlistener) {
+        this.a((PacketPlayInListener) packetlistener);
+    }
+}
---
+++ src/main/java/net/minecraft/server/PacketPlayOutPlayerInfo.java
@@ -0,0 +1,38 @@
+package net.minecraft.server;
+
+import java.io.IOException;
+
+public class PacketPlayOutPlayerInfo extends Packet {
+
+    private String a;
+    private boolean b;
+    private int c;
+
+    public PacketPlayOutPlayerInfo() {}
+
+    public PacketPlayOutPlayerInfo(String s, boolean flag, int i) {
+        this.a = s;
+        this.b = flag;
+        this.c = i;
+    }
+
+    public void a(PacketDataSerializer packetdataserializer) throws IOException {
+        this.a = packetdataserializer.c(16);
+        this.b = packetdataserializer.readBoolean();
+        this.c = packetdataserializer.readShort();
+    }
+
+    public void b(PacketDataSerializer packetdataserializer) throws IOException {
+        packetdataserializer.a(this.a);
+        packetdataserializer.writeBoolean(this.b);
+        packetdataserializer.writeShort(this.c);
+    }
+
+    public void a(PacketPlayOutListener packetplayoutlistener) {
+        packetplayoutlistener.a(this);
+    }
+
+    public void handle(PacketListener packetlistener) {
+        this.a((PacketPlayOutListener) packetlistener);
+    }
+}
